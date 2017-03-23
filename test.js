7cfe828c6dbb8429d151934a79ce7e72a35051fb
cutebill <bestbill.act@gmail.com>
2017-03-23 12:02:35 +0800

    plus sel sort

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const bub = require('./bubsort/bubsort')
 const insert = require('./insertsort/insertsort')
+const sel = require('./selsort/selsort')
 function random ({min = 0, max = 100, arrSize = 100} = {}) {
   const data = []
   for (let i = 0; i < arrSize; i++) {
@@ -13,7 +14,8 @@ function main () {
   const ARRSIZE = 100
   const data = random({min: MIN, max: MAX, arrSize: ARRSIZE})
   // const sortData = bub.sort({data: data})
-  const sortData = insert.sort({data: data})
+  // const sortData = insert.sort({data: data})
+  const sortData = sel.sort({data: data})
   console.log(sortData)
 }
 
